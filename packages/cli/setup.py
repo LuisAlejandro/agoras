4b5c2c96228c9a7fd2e5e04b3474ba61b5d5e0fd
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2026-06-22 21:13:45 -0400

    Updating Changelog and version to 2.0.2

--- packages/cli/setup.py
+++ packages/cli/setup.py
@@ -21,7 +21,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name='agoras',
-    version='2.0.1',
+    version='2.0.2',
     author='Luis Alejandro Martínez Faneyth',
     author_email='luis@luisalejandro.org',
     url='https://github.com/LuisAlejandro/agoras',
